a98d3df8fb6238ebd0377ce97560f614a402a644
Eugene Rymarev <EugeneRymarev@users.noreply.github.com>
2025-06-15 01:28:17 +0500

    reformat formatting tests code

--- openpyxl/formatting/tests/test_formatting.py
+++ openpyxl/formatting/tests/test_formatting.py
@@ -1,36 +1,33 @@
 # Copyright (c) 2010-2025 openpyxl
-# package imports
 import pytest
 from openpyxl.formatting.formatting import ConditionalFormattingList
-from openpyxl.formatting.rule import CellIsRule
-from openpyxl.formatting.rule import FormulaRule
-from openpyxl.formatting.rule import Rule
 from openpyxl.reader.excel import load_workbook
-from openpyxl.styles import Border
-from openpyxl.styles import borders
-from openpyxl.styles import Color
-from openpyxl.styles import colors
-from openpyxl.styles import fills
-from openpyxl.styles import Font
-from openpyxl.styles import PatternFill
-from openpyxl.styles import Side
+from openpyxl.styles.borders import Border
+from openpyxl.styles.borders import Side
+from openpyxl.styles.colors import Color
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl.styles.differential import DifferentialStyleList
+from openpyxl.styles.fills import PatternFill
+from openpyxl.styles.fonts import Font
 from openpyxl.tests.helper import compare_xml
 from openpyxl.xml.functions import fromstring
 from openpyxl.xml.functions import tostring
-# test imports
+
+
+@pytest.fixture
+def conditional_formatting():
+    from ..formatting import ConditionalFormatting
+
+    return ConditionalFormatting
 
 
 class DummyWorkbook:
-
     def __init__(self):
         self._differential_styles = DifferentialStyleList()
         self.worksheets = []
 
 
 class DummyWorksheet:
-
     def __init__(self):
         self.conditional_formatting = ConditionalFormattingList()
         self.parent = DummyWorkbook()
@@ -43,166 +40,124 @@ def test_conditional_formatting_read(datadir):
     ws = wb.active
     rules = ws.conditional_formatting
     assert len(rules) == 30
-
     # First test the conditional formatting rules read
     rule = rules["A1:A1048576"][0]
-    assert dict(rule) == {
-        "priority": "30",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "30", "type": "colorScale"}
     rule = rules["B1:B10"][0]
     assert dict(rule) == {"priority": "29", "type": "colorScale"}
-
     rule = rules["C1:C10"][0]
     assert dict(rule) == {"priority": "28", "type": "colorScale"}
-
     rule = rules["D1:D10"][0]
-    assert dict(rule) == {
-        "priority": "27",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "27", "type": "colorScale"}
     rule = rules["E1:E10"][0]
-    assert dict(rule) == {
-        "priority": "26",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "26", "type": "colorScale"}
     rule = rules["F1:F10"][0]
-    assert dict(rule) == {
-        "priority": "25",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "25", "type": "colorScale"}
     rule = rules["G1:G10"][0]
-    assert dict(rule) == {
-        "priority": "24",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "24", "type": "colorScale"}
     rule = rules["H1:H10"][0]
-    assert dict(rule) == {
-        "priority": "23",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "23", "type": "colorScale"}
     rule = rules["I1:I10"][0]
-    assert dict(rule) == {
-        "priority": "22",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "22", "type": "colorScale"}
     rule = rules["J1:J10"][0]
-    assert dict(rule) == {
-        "priority": "21",
-        "type": "colorScale",
-    }
-
+    assert dict(rule) == {"priority": "21", "type": "colorScale"}
     rule = rules["K1:K10"][0]
     assert dict(rule) == {"priority": "20", "type": "dataBar"}
-
     rule = rules["L1:L10"][0]
     assert dict(rule) == {"priority": "19", "type": "dataBar"}
-
     rule = rules["M1:M10"][0]
     assert dict(rule) == {"priority": "18", "type": "dataBar"}
-
     rule = rules["N1:N10"][0]
     assert dict(rule) == {"priority": "17", "type": "iconSet"}
-
     rule = rules["O1:O10"][0]
     assert dict(rule) == {"priority": "16", "type": "iconSet"}
-
     rule = rules["P1:P10"][0]
     assert dict(rule) == {"priority": "15", "type": "iconSet"}
-
     rule = rules["Q1:Q10"][0]
-    assert dict(rule) == {
+    expected = {
         "text": "3",
         "priority": "14",
         "dxfId": "27",
         "operator": "containsText",
         "type": "containsText",
     }
-    assert rule.dxf == DifferentialStyle(
-        font=Font(color="FF9C0006"), fill=PatternFill(bgColor="FFFFC7CE")
+    assert dict(rule) == expected
+    expected = DifferentialStyle(
+        font=Font(color="FF9C0006"),
+        fill=PatternFill(bgColor="FFFFC7CE"),
     )
-
+    assert rule.dxf == expected
     rule = rules["R1:R10"][0]
-    assert dict(rule) == {
+    expected = {
         "operator": "between",
         "dxfId": "26",
         "type": "cellIs",
         "priority": "13",
     }
-    assert rule.dxf == DifferentialStyle(
-        font=Font(color="FF9C6500"), fill=PatternFill(bgColor="FFFFEB9C")
+    assert dict(rule) == expected
+    expected = DifferentialStyle(
+        font=Font(color="FF9C6500"),
+        fill=PatternFill(bgColor="FFFFEB9C"),
     )
-
+    assert rule.dxf == expected
     rule = rules["S1:S10"][0]
-    assert dict(rule) == {
+    expected = {
         "priority": "12",
         "dxfId": "25",
         "percent": "1",
         "type": "top10",
         "rank": "10",
     }
-
+    assert dict(rule) == expected
     rule = rules["T1:T10"][0]
-    assert dict(rule) == {
+    expected = {
         "priority": "11",
         "dxfId": "24",
         "type": "top10",
         "rank": "4",
         "bottom": "1",
     }
-
+    assert dict(rule) == expected
     rule = rules["U1:U10"][0]
     assert dict(rule) == {"priority": "10", "dxfId": "23", "type": "aboveAverage"}
-
     rule = rules["V1:V10"][0]
-    assert dict(rule) == {
+    expected = {
         "aboveAverage": "0",
         "dxfId": "22",
         "type": "aboveAverage",
         "priority": "9",
     }
-
+    assert dict(rule) == expected
     rule = rules["W1:W10"][0]
-    assert dict(rule) == {
+    expected = {
         "priority": "8",
         "dxfId": "21",
         "type": "aboveAverage",
         "equalAverage": "1",
     }
-
+    assert dict(rule) == expected
     rule = rules["X1:X10"][0]
-    assert dict(rule) == {
+    expected = {
         "aboveAverage": "0",
         "dxfId": "20",
         "priority": "7",
         "type": "aboveAverage",
         "equalAverage": "1",
     }
-
+    assert dict(rule) == expected
     rule = rules["Y1:Y10"][0]
-    assert dict(rule) == {
-        "priority": "6",
-        "dxfId": "19",
-        "type": "aboveAverage",
-        "stdDev": "1",
-    }
-
+    expected = {"priority": "6", "dxfId": "19", "type": "aboveAverage", "stdDev": "1"}
+    assert dict(rule) == expected
     rule = rules["Z1:Z10"][0]
-    assert dict(rule) == {
+    expected = {
         "aboveAverage": "0",
         "dxfId": "18",
         "type": "aboveAverage",
         "stdDev": "1",
         "priority": "5",
     }
-    assert rule.dxf == DifferentialStyle(
+    assert dict(rule) == expected
+    expected = DifferentialStyle(
         font=Font(b=True, i=True, color="FF9C0006"),
         fill=PatternFill(bgColor="FFFFC7CE"),
         border=Border(
@@ -212,68 +167,46 @@ def test_conditional_formatting_read(datadir):
             bottom=Side(style="thin", color=Color(theme=5)),
         ),
     )
-
+    assert rule.dxf == expected
     rule = rules["AA1:AA10"][0]
-    assert dict(rule) == {
-        "priority": "4",
-        "dxfId": "17",
-        "type": "aboveAverage",
-        "stdDev": "2",
-    }
-
+    expected = {"priority": "4", "dxfId": "17", "type": "aboveAverage", "stdDev": "2"}
+    assert dict(rule) == expected
     rule = rules["AB1:AB10"][0]
     assert dict(rule) == {"priority": "3", "dxfId": "16", "type": "duplicateValues"}
-
     rule = rules["AC1:AC10"][0]
     assert dict(rule) == {"priority": "2", "dxfId": "15", "type": "uniqueValues"}
-
     rule = rules["AD1:AD10"][0]
-    assert dict(rule) == {
-        "priority": "1",
-        "dxfId": "14",
-        "type": "expression",
-    }
-
-
-@pytest.fixture
-def ConditionalFormatting():
-    from ..formatting import ConditionalFormatting
-
-    return ConditionalFormatting
+    expected = {"priority": "1", "dxfId": "14", "type": "expression"}
+    assert dict(rule) == expected
 
 
 class TestConditionalFormatting:
-
-    def test_ctor(self, ConditionalFormatting):
-        cf = ConditionalFormatting(sqref="A1:B5")
+    def test_ctor(self, conditional_formatting):
+        cf = conditional_formatting(sqref="A1:B5")
         xml = tostring(cf.to_tree())
-        expected = """
-        <conditionalFormatting sqref="A1:B5" />
-        """
+        expected = '<conditionalFormatting sqref="A1:B5"/>'
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_from_tree(self, ConditionalFormatting):
-        src = """
-        <conditionalFormatting sqref="A1:B5" />
-        """
+    def test_from_tree(self, conditional_formatting):
+        src = '<conditionalFormatting sqref="A1:B5"/>'
         tree = fromstring(src)
-        cf = ConditionalFormatting.from_tree(tree)
+        cf = conditional_formatting.from_tree(tree)
         assert cf.sqref == "A1:B5"
 
-    def test_eq(self, ConditionalFormatting):
-        c1 = ConditionalFormatting("A1:B5")
-        c2 = ConditionalFormatting("A1:B5", pivot=True)
+    def test_eq(self, conditional_formatting):
+        c1 = conditional_formatting("A1:B5")
+        c2 = conditional_formatting("A1:B5", pivot=True)
         assert c1 == c2
 
-    def test_hash(self, ConditionalFormatting):
-        c1 = ConditionalFormatting("A1:B5")
+    def test_hash(self, conditional_formatting):
+        c1 = conditional_formatting("A1:B5")
         assert hash(c1) == hash("A1:B5")
 
-    def test_repr(self, ConditionalFormatting):
-        c1 = ConditionalFormatting("A1:B5")
+    def test_repr(self, conditional_formatting):
+        c1 = conditional_formatting("A1:B5")
         assert repr(c1) == "<ConditionalFormatting A1:B5>"
 
-    def test_contains(self, ConditionalFormatting):
-        c2 = ConditionalFormatting("A1:A5 B1:B5")
+    def test_contains(self, conditional_formatting):
+        c2 = conditional_formatting("A1:A5 B1:B5")
         assert "B2" in c2
--- openpyxl/formatting/tests/test_rule.py
+++ openpyxl/formatting/tests/test_rule.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2010-2025 openpyxl
 import pytest
-from openpyxl.styles import Color
+from openpyxl.styles.colors import Color
 from openpyxl.tests.helper import compare_xml
 from openpyxl.xml.constants import SHEET_MAIN_NS
 from openpyxl.xml.functions import fromstring
@@ -8,25 +8,59 @@ from openpyxl.xml.functions import tostring
 
 
 @pytest.fixture
-def FormatObject():
-    from ..rule import FormatObject
+def format_object():
+    from openpyxl.formatting.rule import FormatObject
 
     return FormatObject
 
 
-class TestFormatObject:
+@pytest.fixture
+def color_scale():
+    from openpyxl.formatting.rule import ColorScale
 
-    def test_create(self, FormatObject):
-        xml = fromstring("""<cfvo type="num" val="3"/>""")
-        cfvo = FormatObject.from_tree(xml)
+    return ColorScale
+
+
+@pytest.fixture
+def color_scale_rule():
+    from openpyxl.formatting.rule import ColorScaleRule
+
+    return ColorScaleRule
+
+
+@pytest.fixture
+def data_bar():
+    from openpyxl.formatting.rule import DataBar
+
+    return DataBar
+
+
+@pytest.fixture
+def icon_set():
+    from openpyxl.formatting.rule import IconSet
+
+    return IconSet
+
+
+@pytest.fixture
+def rule():
+    from openpyxl.formatting.rule import Rule
+
+    return Rule
+
+
+class TestFormatObject:
+    def test_create(self, format_object):
+        xml = fromstring('<cfvo type="num" val="3"/>')
+        cfvo = format_object.from_tree(xml)
         assert cfvo.type == "num"
         assert cfvo.val == 3
         assert cfvo.gte is None
 
-    def test_serialise(self, FormatObject):
-        cfvo = FormatObject(type="percent", val=4)
+    def test_serialise(self, format_object):
+        cfvo = format_object(type="percent", val=4)
         xml = tostring(cfvo.to_tree())
-        expected = """<cfvo type="percent" val="4"/>"""
+        expected = '<cfvo type="percent" val="4"/>'
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
@@ -42,72 +76,56 @@ class TestFormatObject:
             ("formula", None, None),
         ],
     )
-    def test_value_types(self, FormatObject, typ, value, expected):
-        cfvo = FormatObject(type=typ, val=value)
+    def test_value_types(self, format_object, typ, value, expected):
+        cfvo = format_object(type=typ, val=value)
         assert cfvo.val == expected
 
-    def test_cell_reference(self, FormatObject):
-        cfvo = FormatObject(type="num")
+    def test_cell_reference(self, format_object):
+        cfvo = format_object(type="num")
         cfvo.val = "$K$6"
         assert cfvo.val == "$K$6"
 
 
-@pytest.fixture
-def ColorScale():
-    from ..rule import ColorScale
-
-    return ColorScale
-
-
-@pytest.fixture
-def ColorScaleRule():
-    from ..rule import ColorScaleRule
-
-    return ColorScaleRule
-
-
 class TestColorScale:
-
-    def test_create(self, ColorScale):
+    def test_create(self, color_scale):
         src = """
-        <colorScale xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-        <cfvo type="min"/>
-        <cfvo type="max"/>
-        <color rgb="FFFF7128"/>
-        <color rgb="FFFFEF9C"/>
+        <colorScale
+                xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+            <cfvo type="min"/>
+            <cfvo type="max"/>
+            <color rgb="FFFF7128"/>
+            <color rgb="FFFFEF9C"/>
         </colorScale>
         """
         xml = fromstring(src)
-        cs = ColorScale.from_tree(xml)
+        cs = color_scale.from_tree(xml)
         assert len(cs.cfvo) == 2
         assert len(cs.color) == 2
 
-    def test_serialise(self, ColorScale, FormatObject):
-        fo1 = FormatObject(type="min", val="0")
-        fo2 = FormatObject(type="percent", val="50")
-        fo3 = FormatObject(type="max", val="0")
-
+    def test_serialise(self, color_scale, format_object):
+        fo1 = format_object(type="min", val="0")
+        fo2 = format_object(type="percent", val="50")
+        fo3 = format_object(type="max", val="0")
         col1 = Color(rgb="FFFF0000")
         col2 = Color(rgb="FFFFFF00")
         col3 = Color(rgb="FF00B050")
-
-        cs = ColorScale(cfvo=[fo1, fo2, fo3], color=[col1, col2, col3])
+        cs = color_scale(cfvo=[fo1, fo2, fo3], color=[col1, col2, col3])
         xml = tostring(cs.to_tree())
         expected = """
         <colorScale>
-        <cfvo type="min" val="0"/>
-        <cfvo type="percent" val="50"/>
-        <cfvo type="max" val="0"/>
-        <color rgb="FFFF0000"/>
-        <color rgb="FFFFFF00"/>
-        <color rgb="FF00B050"/>
+            <cfvo type="min" val="0"/>
+            <cfvo type="percent" val="50"/>
+            <cfvo type="max" val="0"/>
+            <color rgb="FFFF0000"/>
+            <color rgb="FFFFFF00"/>
+            <color rgb="FF00B050"/>
         </colorScale>
         """
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_two_colors(self, ColorScaleRule):
-        cfRule = ColorScaleRule(
+    def test_two_colors(self, color_scale_rule):
+        cfRule = color_scale_rule(
             start_type="min",
             start_value=None,
             start_color="FFAA0000",
@@ -118,19 +136,19 @@ class TestColorScale:
         xml = tostring(cfRule.to_tree())
         expected = """
         <cfRule priority="0" type="colorScale">
-          <colorScale>
-            <cfvo type="min"/>
-            <cfvo type="max"/>
-            <color rgb="FFAA0000"/>
-            <color rgb="FF00AA00"/>
-          </colorScale>
+            <colorScale>
+                <cfvo type="min"/>
+                <cfvo type="max"/>
+                <color rgb="FFAA0000"/>
+                <color rgb="FF00AA00"/>
+            </colorScale>
         </cfRule>
         """
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_three_colors(self, ColorScaleRule):
-        cfRule = ColorScaleRule(
+    def test_three_colors(self, color_scale_rule):
+        cfRule = color_scale_rule(
             start_type="percentile",
             start_value=10,
             start_color="FFAA0000",
@@ -145,12 +163,12 @@ class TestColorScale:
         expected = """
         <cfRule priority="0" type="colorScale">
             <colorScale>
-              <cfvo type="percentile" val="10"></cfvo>
-              <cfvo type="percentile" val="50"></cfvo>
-              <cfvo type="percentile" val="90"></cfvo>
-              <color rgb="FFAA0000"></color>
-              <color rgb="FF0000AA"></color>
-              <color rgb="FF00AA00"></color>
+                <cfvo type="percentile" val="10"></cfvo>
+                <cfvo type="percentile" val="50"></cfvo>
+                <cfvo type="percentile" val="90"></cfvo>
+                <color rgb="FFAA0000"></color>
+                <color rgb="FF0000AA"></color>
+                <color rgb="FF00AA00"></color>
             </colorScale>
         </cfRule>
         """
@@ -158,76 +176,66 @@ class TestColorScale:
         assert diff is None, diff
 
 
-@pytest.fixture
-def DataBar():
-    from ..rule import DataBar
-
-    return DataBar
-
-
 class TestDataBar:
-
-    def test_create(self, DataBar):
+    def test_create(self, data_bar):
         src = """
         <dataBar xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-        <cfvo type="min"/>
-        <cfvo type="max"/>
-        <color rgb="FF638EC6"/>
+            <cfvo type="min"/>
+            <cfvo type="max"/>
+            <color rgb="FF638EC6"/>
         </dataBar>
         """
         xml = fromstring(src)
-        db = DataBar.from_tree(xml)
+        db = data_bar.from_tree(xml)
         assert len(db.cfvo) == 2
         assert db.color.value == "FF638EC6"
 
-    def test_serialise(self, DataBar, FormatObject):
-        fo1 = FormatObject(type="min", val="0")
-        fo2 = FormatObject(type="percent", val="50")
-        db = DataBar(
-            minLength=4, maxLength=10, cfvo=[fo1, fo2], color="FF2266", showValue=True
+    def test_serialise(self, data_bar, format_object):
+        fo1 = format_object(type="min", val="0")
+        fo2 = format_object(type="percent", val="50")
+        db = data_bar(
+            minLength=4,
+            maxLength=10,
+            cfvo=[fo1, fo2],
+            color="FF2266",
+            showValue=True,
         )
         xml = tostring(db.to_tree())
         expected = """
         <dataBar maxLength="10" minLength="4" showValue="1">
-          <cfvo type="min" val="0"></cfvo>
-          <cfvo type="percent" val="50"></cfvo>
-          <color rgb="00FF2266"></color>
-         </dataBar>
+            <cfvo type="min" val="0"></cfvo>
+            <cfvo type="percent" val="50"></cfvo>
+            <color rgb="00FF2266"></color>
+        </dataBar>
         """
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
 
-@pytest.fixture
-def IconSet():
-    from ..rule import IconSet
-
-    return IconSet
-
-
 class TestIconSet:
-
-    def test_create(self, IconSet):
+    def test_create(self, icon_set):
         src = """
-        <iconSet iconSet="5Rating" xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
-          <cfvo type="percent" val="0"/>
-          <cfvo type="percentile" val="20"/>
-          <cfvo type="percentile" val="40"/>
-          <cfvo type="percentile" val="60"/>
-          <cfvo type="percentile" val="80"/>
+        <iconSet
+                iconSet="5Rating"
+                xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
+            <cfvo type="percent" val="0"/>
+            <cfvo type="percentile" val="20"/>
+            <cfvo type="percentile" val="40"/>
+            <cfvo type="percentile" val="60"/>
+            <cfvo type="percentile" val="80"/>
         </iconSet>
         """
         xml = fromstring(src)
-        icon = IconSet.from_tree(xml)
+        icon = icon_set.from_tree(xml)
         assert icon.iconSet == "5Rating"
         assert len(icon.cfvo) == 5
 
-    def test_serialise(self, IconSet, FormatObject):
-        fo1 = FormatObject(type="num", val="2")
-        fo2 = FormatObject(type="num", val="4")
-        fo3 = FormatObject(type="num", val="6")
-        fo4 = FormatObject(type="percent", val="0")
-        icon = IconSet(
+    def test_serialise(self, icon_set, format_object):
+        fo1 = format_object(type="num", val="2")
+        fo2 = format_object(type="num", val="4")
+        fo3 = format_object(type="num", val="6")
+        fo4 = format_object(type="percent", val="0")
+        icon = icon_set(
             cfvo=[fo1, fo2, fo3, fo4],
             iconSet="4ArrowsGray",
             reverse=True,
@@ -236,76 +244,60 @@ class TestIconSet:
         xml = tostring(icon.to_tree())
         expected = """
         <iconSet iconSet="4ArrowsGray" showValue="0" reverse="1">
-          <cfvo type="num" val="2"/>
-          <cfvo type="num" val="4"/>
-          <cfvo type="num" val="6"/>
-          <cfvo type="percent" val="0"/>
+            <cfvo type="num" val="2"/>
+            <cfvo type="num" val="4"/>
+            <cfvo type="num" val="6"/>
+            <cfvo type="percent" val="0"/>
         </iconSet>
         """
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
 
-@pytest.fixture
-def Rule():
-    from ..rule import Rule
-
-    return Rule
-
-
 class TestRule:
-
-    def test_create(self, Rule, datadir):
+    def test_create(self, rule, datadir):
         datadir.chdir()
         with open("worksheet.xml") as src:
             xml = fromstring(src.read())
-
         rules = []
-        for el in xml.findall(
-            "{%s}conditionalFormatting/{%s}cfRule" % (SHEET_MAIN_NS, SHEET_MAIN_NS)
-        ):
-            rules.append(Rule.from_tree(el))
-
+        s = f"{{{SHEET_MAIN_NS}}}conditionalFormatting/{{{SHEET_MAIN_NS}}}cfRule"
+        for el in xml.findall(s):
+            rules.append(rule.from_tree(el))
         assert len(rules) == 30
         assert rules[17].formula == ["2", "7"]
-        assert rules[-1].formula == [
-            "AD1>3",
-        ]
+        assert rules[-1].formula == ["AD1>3"]
 
-    def test_serialise(self, Rule):
-
-        rule = Rule(type="cellIs", dxfId="26", priority="13", operator="between")
-        rule.formula = ["2", "7"]
-
-        xml = tostring(rule.to_tree())
+    def test_serialise(self, rule):
+        r = rule(type="cellIs", dxfId="26", priority="13", operator="between")
+        r.formula = ["2", "7"]
+        xml = tostring(r.to_tree())
         expected = """
         <cfRule type="cellIs" dxfId="26" priority="13" operator="between">
-        <formula>2</formula>
-        <formula>7</formula>
+            <formula>2</formula>
+            <formula>7</formula>
         </cfRule>
         """
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_non_ascii_formula(self, Rule):
-
-        rule = Rule(
-            type="cellIs", priority=10, formula=[b"D\xc3\xbcsseldorf".decode("utf-8")]
+    def test_non_ascii_formula(self, rule):
+        r = rule(
+            type="cellIs",
+            priority=10,
+            formula=[b"D\xc3\xbcsseldorf".decode("utf-8")],
         )
-
-        xml = tostring(rule.to_tree())
+        xml = tostring(r.to_tree())
         expected = b"""
         <cfRule priority="10" type="cellIs">
-          <formula>D\xc3\xbcsseldorf</formula>
+            <formula>D\xc3\xbcsseldorf</formula>
         </cfRule>
         """
         diff = compare_xml(xml, expected)
-
         assert diff is None, diff
 
 
 def test_formula_rule():
-    from ..rule import FormulaRule
+    from openpyxl.formatting.rule import FormulaRule
     from openpyxl.styles.differential import DifferentialStyle
 
     cf = FormulaRule(formula=["ISBLANK(C1)"], stopIfTrue=True)
@@ -315,22 +307,24 @@ def test_formula_rule():
 
 
 def test_cellis_rule():
-    from ..rule import CellIsRule
-    from openpyxl.styles import PatternFill
+    from openpyxl.formatting.rule import CellIsRule
+    from openpyxl.styles.fills import PatternFill
 
     red_fill = PatternFill(
-        start_color="FFEE1111", end_color="FFEE1111", fill_type="solid"
+        start_color="FFEE1111",
+        end_color="FFEE1111",
+        fill_type="solid",
     )
-
-    rule = CellIsRule(operator="<", formula=["C$1"], stopIfTrue=True, fill=red_fill)
-    assert dict(rule) == {
+    r = CellIsRule(operator="<", formula=["C$1"], stopIfTrue=True, fill=red_fill)
+    expected = {
         "operator": "lessThan",
         "priority": "0",
         "type": "cellIs",
         "stopIfTrue": "1",
     }
-    assert rule.formula == ["C$1"]
-    assert rule.dxf.fill == red_fill
+    assert dict(r) == expected
+    assert r.formula == ["C$1"]
+    assert r.dxf.fill == red_fill
 
 
 @pytest.mark.parametrize(
@@ -346,7 +340,7 @@ def test_cellis_rule():
     ],
 )
 def test_operator_expansion(value, expansion):
-    from ..rule import CellIsRule
+    from openpyxl.formatting.rule import CellIsRule
 
     cf1 = CellIsRule(operator=value, formula=[])
     cf2 = CellIsRule(operator=expansion, formula=[])
@@ -355,19 +349,19 @@ def test_operator_expansion(value, expansion):
 
 
 def test_iconset_rule():
-    from ..rule import IconSetRule
+    from openpyxl.formatting.rule import IconSetRule
 
-    rule = IconSetRule("5Arrows", "percent", [10, 20, 30, 40, 50])
-    xml = tostring(rule.to_tree())
+    r = IconSetRule("5Arrows", "percent", [10, 20, 30, 40, 50])
+    xml = tostring(r.to_tree())
     expected = """
     <cfRule priority="0" type="iconSet">
-    <iconSet iconSet="5Arrows">
-      <cfvo type="percent" val="10"/>
-      <cfvo type="percent" val="20"/>
-      <cfvo type="percent" val="30"/>
-      <cfvo type="percent" val="40"/>
-      <cfvo type="percent" val="50"/>
-    </iconSet>
+        <iconSet iconSet="5Arrows">
+            <cfvo type="percent" val="10"/>
+            <cfvo type="percent" val="20"/>
+            <cfvo type="percent" val="30"/>
+            <cfvo type="percent" val="40"/>
+            <cfvo type="percent" val="50"/>
+        </iconSet>
     </cfRule>
     """
     diff = compare_xml(xml, expected)
@@ -375,23 +369,23 @@ def test_iconset_rule():
 
 
 def test_databar_rule():
-    from ..rule import DataBarRule
+    from openpyxl.formatting.rule import DataBarRule
 
-    rule = DataBarRule(
+    r = DataBarRule(
         start_type="percentile",
         start_value=10,
         end_type="percentile",
         end_value="90",
         color="FF638EC6",
     )
-    xml = tostring(rule.to_tree())
+    xml = tostring(r.to_tree())
     expected = """
     <cfRule type="dataBar" priority="0">
-    <dataBar>
-      <cfvo type="percentile" val="10"/>
-      <cfvo type="percentile" val="90"/>
-      <color rgb="FF638EC6"/>
-    </dataBar>
+        <dataBar>
+            <cfvo type="percentile" val="10"/>
+            <cfvo type="percentile" val="90"/>
+            <color rgb="FF638EC6"/>
+        </dataBar>
     </cfRule>
     """
     diff = compare_xml(xml, expected)
