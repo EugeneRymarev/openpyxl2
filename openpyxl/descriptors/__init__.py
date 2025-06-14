e8905061fc05b6aabb577dbc529796f1c23f9d9e
Eugene Rymarev <EugeneRymarev@users.noreply.github.com>
2025-06-14 17:37:10 +0500

    reformat descriptors code [10]

--- openpyxl/descriptors/__init__.py
+++ openpyxl/descriptors/__init__.py
@@ -14,6 +14,7 @@ from openpyxl.descriptors.base import Max
 from openpyxl.descriptors.base import Min
 from openpyxl.descriptors.base import MinMax
 from openpyxl.descriptors.base import Set
+from openpyxl.descriptors.base import DateTime
 
 
 
