8f6c3ddadfa2bd96a8539fd36edee92f78e04d6a
Eugene Rymarev <EugeneRymarev@users.noreply.github.com>
2025-06-15 23:24:07 +0500

    reformat volatile code

--- openpyxl/volatile/volatile.py
+++ openpyxl/volatile/volatile.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2010-2025 openpyxl
-from openpyxl.descriptors import Integer
-from openpyxl.descriptors import NoneSet
-from openpyxl.descriptors import Set
-from openpyxl.descriptors import String
-from openpyxl.descriptors import Typed
+from openpyxl.descriptors.base import Integer
+from openpyxl.descriptors.base import NoneSet
+from openpyxl.descriptors.base import Set
+from openpyxl.descriptors.base import String
+from openpyxl.descriptors.base import Typed
 from openpyxl.descriptors.excel import ExtensionList
 from openpyxl.descriptors.nested import NestedText
 from openpyxl.descriptors.sequence import Sequence
@@ -12,47 +12,24 @@ from openpyxl.xml.constants import SHEET_MAIN_NS
 
 
 class VolTopicRef(Serialisable):
-
     tagname = "tr"
-
     r = String()
     s = Integer()
 
-    def __init__(
-        self,
-        r=None,
-        s=None,
-    ):
+    def __init__(self, r=None, s=None):
         self.r = r
         self.s = s
 
 
 class VolTopic(Serialisable):
-
     tagname = "tp"
-
-    v = NestedText(
-        expected_type=str,
-    )
+    v = NestedText(expected_type=str)
     stp = NestedText(expected_type=str, allow_none=True)
-    tr = Sequence(
-        expected_type=VolTopicRef,
-    )
+    tr = Sequence(expected_type=VolTopicRef)
     t = NoneSet(values=(["b", "n", "e", "s"]))
+    __elements__ = ("v", "stp", "tr")
 
-    __elements__ = (
-        "v",
-        "stp",
-        "tr",
-    )
-
-    def __init__(
-        self,
-        v=None,
-        stp=None,
-        tr=(),
-        t="n",
-    ):
+    def __init__(self, v=None, stp=None, tr=(), t="n"):
         self.v = v
         self.stp = stp
         self.tr = tr
@@ -60,52 +37,32 @@ class VolTopic(Serialisable):
 
 
 class VolMain(Serialisable):
-
     tagname = "main"
-
-    tp = Sequence(
-        expected_type=VolTopic,
-    )
+    tp = Sequence(expected_type=VolTopic)
     first = String()
 
-    def __init__(
-        self,
-        tp=(),
-        first=None,
-    ):
+    def __init__(self, tp=(), first=None):
         self.tp = tp
         self.first = first
 
 
 class VolType(Serialisable):
-
     tagname = "volType"
-
     main = Sequence(expected_type=VolMain)
     type = Set(values=(["realTimeData", "olapFunctions"]))
-
     __elements__ = ("main",)
 
-    def __init__(
-        self,
-        main=(),
-        type=None,
-    ):
+    def __init__(self, main=(), type=None):
         self.main = main
         self.type = type
 
 
 class VolTypesList(Serialisable):
-
     tagname = "volTypes"
     _path = "/xl/volatileDependencies.xml"
     mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.volatileDependencies+xml"
-
-    volType = Sequence(
-        expected_type=VolType,
-    )
+    volType = Sequence(expected_type=VolType)
     extLst = Typed(expected_type=ExtensionList, allow_none=True)
-
     __elements__ = ("volType", "extLst")
 
     def __init__(self, volType=(), extLst=None):
