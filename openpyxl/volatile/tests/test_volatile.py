2c3444a2ae4b4f2f227b540b05735362fb13cf72
Eugene Rymarev <EugeneRymarev@users.noreply.github.com>
2025-06-15 23:23:39 +0500

    reformat volatile tests code

--- openpyxl/volatile/tests/test_volatile.py
+++ openpyxl/volatile/tests/test_volatile.py
@@ -6,52 +6,65 @@ from openpyxl.xml.functions import tostring
 
 
 @pytest.fixture
-def VolTopicRef():
-    from ..volatile import VolTopicRef
+def vol_topic_ref():
+    from openpyxl.volatile.volatile import VolTopicRef
 
     return VolTopicRef
 
 
-class TestVolTopicRef:
-
-    def test_ctor(self, VolTopicRef):
-        ref = VolTopicRef(r="A1", s=3)
-        xml = tostring(ref.to_tree())
-        expected = """
-        <tr r="A1" s="3" />
-        """
-        diff = compare_xml(xml, expected)
-        assert diff is None, diff
-
-    def test_from_xml(self, VolTopicRef):
-        src = """
-        <tr r="A1" s="1" />
-        """
-        node = fromstring(src)
-        ref = VolTopicRef.from_tree(node)
-        assert ref == VolTopicRef(r="A1", s=1)
-
-
 @pytest.fixture
-def VolMain():
-    from ..volatile import VolMain
+def vol_main():
+    from openpyxl.volatile.volatile import VolMain
 
     return VolMain
 
 
-class TestVolMain:
+@pytest.fixture
+def vol_type():
+    from openpyxl.volatile.volatile import VolType
 
-    def test_ctor(self, VolMain):
-        main = VolMain(first="ThisDataModel")
-        xml = tostring(main.to_tree())
-        expected = """
-        <main first="ThisDataModel" />
-        """
+    return VolType
+
+
+@pytest.fixture
+def vol_topic():
+    from openpyxl.volatile.volatile import VolTopic
+
+    return VolTopic
+
+
+@pytest.fixture
+def vol_types():
+    from openpyxl.volatile.volatile import VolTypesList
+
+    return VolTypesList
+
+
+class TestVolTopicRef:
+    def test_ctor(self, vol_topic_ref):
+        ref = vol_topic_ref(r="A1", s=3)
+        xml = tostring(ref.to_tree())
+        expected = '<tr r="A1" s="3"/>'
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_from_xml(self, VolMain):
-        from ..volatile import VolTopic
+    def test_from_xml(self, vol_topic_ref):
+        src = '<tr r="A1" s="1"/>'
+        node = fromstring(src)
+        ref = vol_topic_ref.from_tree(node)
+        assert ref == vol_topic_ref(r="A1", s=1)
+
+
+class TestVolMain:
+    def test_ctor(self, vol_main):
+        main = vol_main(first="ThisDataModel")
+        xml = tostring(main.to_tree())
+        expected = '<main first="ThisDataModel"/>'
+        diff = compare_xml(xml, expected)
+        assert diff is None, diff
+
+    def test_from_xml(self, vol_main):
+        from openpyxl.volatile.volatile import VolTopic
 
         src = """
         <main first="ThisWorkbookDataModel">
@@ -61,25 +74,20 @@ class TestVolMain:
         </main>
         """
         node = fromstring(src)
-        main = VolMain.from_tree(node)
-        assert main == VolMain(
-            first="ThisWorkbookDataModel", tp=[VolTopic(t="e", v="#N/A")]
+        main = vol_main.from_tree(node)
+        expected = vol_main(
+            first="ThisWorkbookDataModel",
+            tp=[VolTopic(t="e", v="#N/A")],
         )
-
-
-@pytest.fixture
-def VolType():
-    from ..volatile import VolType
-
-    return VolType
+        assert main == expected
 
 
 class TestVolType:
+    def test_ctor(self, vol_type):
+        from openpyxl.volatile.volatile import VolMain
+        from openpyxl.volatile.volatile import VolTopic
 
-    def test_ctor(self, VolType):
-        from ..volatile import VolMain, VolTopic
-
-        typ = VolType(
+        typ = vol_type(
             main=[VolMain(first="teststring", tp=[VolTopic(t="s", v="aaa: 4447")])],
             type="realTimeData",
         )
@@ -96,35 +104,27 @@ class TestVolType:
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_from_xml(self, VolType):
+    def test_from_xml(self, vol_type):
         src = """
         <volType type="olapFunctions">
             <main first="ThisWorkbookDataModel">
                 <tp t="e">
                     <v>#N/A</v>
                     <stp>1</stp>
-                    <tr r="A1" s="1" />
+                    <tr r="A1" s="1"/>
                 </tp>
             </main>
         </volType>
         """
         node = fromstring(src)
-        typ = VolType.from_tree(node)
+        typ = vol_type.from_tree(node)
         assert typ.type == "olapFunctions"
         assert typ.main[0].first == "ThisWorkbookDataModel"
 
 
-@pytest.fixture
-def VolTopic():
-    from ..volatile import VolTopic
-
-    return VolTopic
-
-
 class TestVolTopic:
-
-    def test_ctor(self, VolTopic):
-        topic = VolTopic(t="s", v="aaa: 4447")
+    def test_ctor(self, vol_topic):
+        topic = vol_topic(t="s", v="aaa: 4447")
         xml = tostring(topic.to_tree())
         expected = """
         <tp t="s">
@@ -134,40 +134,36 @@ class TestVolTopic:
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_from_xml(self, VolTopic):
-        from ..volatile import VolTopicRef
+    def test_from_xml(self, vol_topic):
+        from openpyxl.volatile.volatile import VolTopicRef
 
         src = """
-            <tp t="e">
-                <v>#N/A</v>
-                <stp>1</stp>
-                <tr r="A1" s="1"></tr>
-            </tp>
+        <tp t="e">
+            <v>#N/A</v>
+            <stp>1</stp>
+            <tr r="A1" s="1"></tr>
+        </tp>
         """
         node = fromstring(src)
-        topic = VolTopic.from_tree(node)
-        assert topic == VolTopic(
-            t="e", v="#N/A", stp="1", tr=[VolTopicRef(r="A1", s=1)]
-        )
-
-
-@pytest.fixture
-def VolTypes():
-    from ..volatile import VolTypesList
-
-    return VolTypesList
+        topic = vol_topic.from_tree(node)
+        expected = vol_topic(t="e", v="#N/A", stp="1", tr=[VolTopicRef(r="A1", s=1)])
+        assert topic == expected
 
 
 class TestVolTypes:
+    def test_ctor(self, vol_types):
+        from openpyxl.volatile.volatile import VolMain
+        from openpyxl.volatile.volatile import VolTopic
+        from openpyxl.volatile.volatile import VolType
 
-    def test_ctor(self, VolTypes):
-        from ..volatile import VolMain, VolTopic, VolType
-
-        typ = VolTypes(
+        typ = vol_types(
             volType=[
                 VolType(
                     main=[
-                        VolMain(first="teststring", tp=[VolTopic(t="s", v="aaa: 4447")])
+                        VolMain(
+                            first="teststring",
+                            tp=[VolTopic(t="s", v="aaa: 4447")],
+                        )
                     ],
                     type="realTimeData",
                 )
@@ -179,7 +175,7 @@ class TestVolTypes:
             <volType type="realTimeData">
                 <main first="teststring">
                     <tp t="s">
-                    <v>aaa: 4447</v>
+                        <v>aaa: 4447</v>
                     </tp>
                 </main>
             </volType>
@@ -188,7 +184,7 @@ class TestVolTypes:
         diff = compare_xml(xml, expected)
         assert diff is None, diff
 
-    def test_from_xml(self, VolTypes):
+    def test_from_xml(self, vol_types):
         src = """
         <volTypes xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">
             <volType type="olapFunctions">
@@ -196,13 +192,13 @@ class TestVolTypes:
                     <tp t="e">
                         <v>#N/A</v>
                         <stp>1</stp>
-                        <tr r="A1" s="1" />
+                        <tr r="A1" s="1"/>
                     </tp>
                 </main>
             </volType>
         </volTypes>
         """
         node = fromstring(src)
-        typ = VolTypes.from_tree(node)
+        typ = vol_types.from_tree(node)
         assert typ.volType[0].type == "olapFunctions"
         assert typ.volType[0].main[0].first == "ThisWorkbookDataModel"
