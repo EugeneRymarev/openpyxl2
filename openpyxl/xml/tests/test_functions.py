ec1c8158c739e33dd3f708248d8ef6ba7c72f86f
Eugene Rymarev <EugeneRymarev@users.noreply.github.com>
2025-06-16 23:16:55 +0500

    reformat xml tests code

--- openpyxl/xml/tests/test_functions.py
+++ openpyxl/xml/tests/test_functions.py
@@ -1,22 +1,24 @@
 # Copyright (c) 2010-2025 openpyxl
-from io import BytesIO
+import io
 
 import pytest
-
-from ..functions import fromstring
-from ..functions import iterparse
+from openpyxl.xml.functions import XML_NS
+from openpyxl.xml.functions import Element
+from openpyxl.xml.functions import fromstring
+from openpyxl.xml.functions import iterparse
+from openpyxl.xml.functions import whitespace
 
 
 @pytest.mark.parametrize(
     "xml, tag",
     [
-        ("<root xmlns='http://openpyxl.org/ns' />", "root"),
-        ("<root />", "root"),
+        ("<root xmlns='http://openpyxl.org/ns'/>", "root"),
+        ("<root/>", "root"),
     ],
 )
 def test_localtag(xml, tag):
-    from ..functions import localname
-    from ..functions import fromstring
+    from openpyxl.xml.functions import fromstring
+    from openpyxl.xml.functions import localname
 
     node = fromstring(xml)
     assert localname(node) == tag
@@ -25,12 +27,12 @@ def test_localtag(xml, tag):
 vulnerable_xml_strings = (
     b"""<?xml version="1.0" encoding="ISO-8859-1"?>
             <!DOCTYPE foo [
-            <!ELEMENT foo ANY >
-            <!ENTITY xxe SYSTEM "file:///dev/random" >]>
+            <!ELEMENT foo ANY>
+            <!ENTITY xxe SYSTEM "file:///dev/random">]>
             <foo>&xxe;</foo>""",
     b"""<?xml version="1.0" encoding="UTF-8"?>
           <!DOCTYPE xmlbomb [
-          <!ENTITY a "1234567890" >
+          <!ENTITY a "1234567890">
           <!ENTITY b "&a;&a;&a;&a;&a;&a;&a;&a;">
           <!ENTITY c "&b;&b;&b;&b;&b;&b;&b;&b;">
           <!ENTITY d "&c;&c;&c;&c;&c;&c;&c;&c;">
@@ -38,7 +40,7 @@ vulnerable_xml_strings = (
           <foo>&d;</foo>""",
     b"""<?xml version="1.0" encoding="UTF-8"?>
           <!DOCTYPE test [
-          <!ENTITY % one SYSTEM "http://127.0.0.1:8100/x.xml" >
+          <!ENTITY % one SYSTEM "http://127.0.0.1:8100/x.xml">
           %one;
           ]>""",
     b"""<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
@@ -64,27 +66,25 @@ def test_iterparse(xml_input):
     from defusedxml.common import DefusedXmlException
 
     with pytest.raises(DefusedXmlException):
-        f = BytesIO(xml_input)
+        f = io.BytesIO(xml_input)
         list(iterparse(f))
 
 
 @pytest.mark.lxml_required
 @pytest.mark.parametrize("xml_input", vulnerable_xml_strings)
 def test_iterparse(xml_input):
-    f = BytesIO(xml_input)
+    f = io.BytesIO(xml_input)
     with pytest.raises(ValueError):
         fromstring(f)
 
 
-from ..functions import Element, whitespace, XML_NS
-
-
 @pytest.mark.parametrize(
-    "value, preserve", [("some text", False), ("Some more Text ", True), (" ", False)]
+    "value, preserve",
+    [("some text", False), ("Some more Text ", True), (" ", False)],
 )
 def test_whitespace(value, preserve):
     el = Element("tag")
     el.text = value
     whitespace(el)
-    check = "{%s}space" % XML_NS in el.attrib
+    check = f"{{{XML_NS}}}space" in el.attrib
     assert check is preserve
