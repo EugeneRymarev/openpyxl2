cbb8bf9583c3bbf829f00b8e8fe24649c490bf83
Eugene Rymarev <EugeneRymarev@users.noreply.github.com>
2025-06-19 23:15:30 +0500

    __init__.py import format

--- openpyxl/__init__.py
+++ openpyxl/__init__.py
@@ -3,7 +3,8 @@
 DEBUG = False
 
 from openpyxl.compat.numbers import NUMPY
-from openpyxl.xml import DEFUSEDXML, LXML
+from openpyxl.xml import DEFUSEDXML
+from openpyxl.xml import LXML
 from openpyxl.workbook.workbook import Workbook
 from openpyxl.reader.excel import load_workbook as open
 from openpyxl.reader.excel import load_workbook
